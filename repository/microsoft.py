59796e0f68d3589be4c55593e480fdeb7c3408d4
cttynul <cttynul@outlook.com>
2022-02-12 22:10:51 +0100

    improved repository, new field in xls

--- repository/microsoft.py
+++ repository/microsoft.py
@@ -14,26 +14,31 @@ def get_kb_from_cve(cve):
     for a in affecteds:
         #print(a)
         remediations = a.getElementsByTagName("vuln:Remediations")#[0]
-        cve_scraped = a.getElementsByTagName("vuln:CVE")[0].childNodes[0].data
-        if cve == cve_scraped:
-            for res in remediations:
-                for r in res.getElementsByTagName("vuln:Remediation"):
-                    try:
-                        kb_raw = (r.getElementsByTagName("vuln:Description")[0]).childNodes[0].data
-                        if(kb_raw.isdigit()):
-                            kb = "KB" + kb_raw
-                            #print(kb)
-                        #version = (r.getElementsByTagName("vuln:ProductID")[0]).childNodes[0].data
-                        version = (r.getElementsByTagName("vuln:ProductID")[0]).childNodes[0].data
-                        if(version.startswith("115")): version_effective = "Windows Server 2019"
-                        elif(version.startswith("108")): version_effective = "Windows Server 2016"
-                        elif(version.startswith("104")): version_effective = "Windows Server 2012 R2"
-                        elif(version.startswith("103")): version_effective = "Windows Server 2012"
-                        elif(version.startswith("100")): version_effective = "Windows Server 2008 R2"
-                        elif(version.startswith("9") or version.startswith("102")): version_effective = "Windows Server 2008"
-                        else:version_effective = False
-                        if(version_effective):
-                            el = {"CVE": cve, "Patch": kb, "OS": version_effective}
-                            if el not in result: result.append(el)
-                    except:pass
+        cves_scraped = a.getElementsByTagName("vuln:CVE")
+        titles = a.getElementsByTagName("vuln:Title")
+        #vuln:Title
+        for cve_scraped in cves_scraped:
+            if cve == cve_scraped.childNodes[0].data:
+                cve_title = titles[cves_scraped.index(cve_scraped)].childNodes[0].data
+                for res in remediations:
+                    for r in res.getElementsByTagName("vuln:Remediation"):
+                        if 1:
+                            kb_raw = (r.getElementsByTagName("vuln:Description")[0]).childNodes[0].data
+                            if(kb_raw.isdigit()): kb = "KB" + kb_raw
+                            else: kb = False
+                                #print(kb)
+                            #version = (r.getElementsByTagName("vuln:ProductID")[0]).childNodes[0].data
+                            version = (r.getElementsByTagName("vuln:ProductID")[0]).childNodes[0].data
+                            if(version.startswith("115")): version_effective = "Windows Server 2019"
+                            elif(version.startswith("108")): version_effective = "Windows Server 2016"
+                            elif(version.startswith("104")): version_effective = "Windows Server 2012 R2"
+                            elif(version.startswith("103")): version_effective = "Windows Server 2012"
+                            elif(version.startswith("100")): version_effective = "Windows Server 2008 R2"
+                            elif(version.startswith("9") or version.startswith("102")): version_effective = "Windows Server 2008"
+                            else:version_effective = False
+                            if(version_effective):
+                                if kb:
+                                    el = {"CVE": cve, "Title": cve_title, "Patch": kb, "OS": version_effective}
+                                    if el not in result: result.append(el)
+                        else:pass
     return result
--- repository/redhat.py
+++ repository/redhat.py
@@ -8,9 +8,10 @@ def get_rhsa_from_cve(cve):
     tree = xml.dom.minidom.parseString(r)
     collection = tree.documentElement
     affecteds = collection.getElementsByTagName("AffectedRelease")
+    cve_title = collection.getElementsByTagName("Bugzilla")[0].childNodes[0].data.strip()
     result = []
     for a in affecteds:
         rhsa = a.getElementsByTagName("Advisory")[0].childNodes[0].data
         os = a.getElementsByTagName("ProductName")[0].childNodes[0].data
-        result.append({"CVE": cve, "Patch":rhsa, "OS": os})
+        result.append({"CVE": cve, "Title": cve_title, "Patch":rhsa, "OS": os})
     return result
