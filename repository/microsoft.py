6e3f25305b390e7b38879fcfba39b1490d68d528
cttynul <cttynul@outlook.com>
2022-02-16 08:37:30 +0100

    little fix

--- repository/microsoft.py
+++ repository/microsoft.py
@@ -4,29 +4,29 @@
 import requests, xml.dom.minidom
 #from support import humanize_ms
 
-product_ids = [{"id": "9312", "os": "Windows Server 2008 for 32-bit Systems Service Pack 2"},
-                {"id": "10049", "os": "Windows Server 2008 R2 for x64-based Systems Service Pack 1 (Server Core installation)"},
-                {"id": "10051", "os": "Windows Server 2008 R2 for x64-based Systems Service Pack 1"},
-                {"id": "10287", "os": "Windows Server 2008 for 32-bit Systems Service Pack 2 (Server Core installation)"},
-                {"id": "10378", "os": "Windows Server 2012"},
+product_ids = [{"id": "93", "os": "Windows Server 2008 for 32-bit Systems Service Pack 2"},
+                {"id": "100", "os": "Windows Server 2008 R2 for x64-based Systems Service Pack 1 (Server Core installation)"},
+                {"id": "100", "os": "Windows Server 2008 R2 for x64-based Systems Service Pack 1"},
+                {"id": "102", "os": "Windows Server 2008 for 32-bit Systems Service Pack 2 (Server Core installation)"},
+                {"id": "103", "os": "Windows Server 2012"},
                 #{"id": "10379", "os": "Windows Server 2012 (Server Core installation)"},
-                {"id": "10483", "os": "Windows Server 2012 R2"},
+                {"id": "104", "os": "Windows Server 2012 R2"},
                 #{"id": "10500", "os": "Windows Server 2012 R2 (Server Core installation)"}, #custom
-                #{"id": "10543", "os": "Windows Server 2012 R2 (Server Core installation)"},
-                {"id": "10816", "os": "Windows Server 2016"},
+                {"id": "105", "os": "Windows Server 2012 R2 (Server Core installation)"},
+                {"id": "108", "os": "Windows Server 2016"},
                 #{"id": "10700", "os": "Windows Server 2016"},#custom
                 #{"id": "10600", "os": "Windows Server 2016"},#custom
                 #{"id": "10855", "os": "Windows Server 2016 (Server Core installation)"},
-                {"id": "11571", "os": "Windows Server 2019"},
+                {"id": "115", "os": "Windows Server 2019"},
                 #{"id": "11600", "os": "Windows Server 2019"}, #custom
                 #{"id": "11700", "os": "Windows Server 2019"}, #custom
                 #{"id": "11572", "os": "Windows Server 2019 (Server Core installation)"},
-                {"id": "11803", "os": "Windows Server, version 20H2 (Server Core Installation)"},
-                {"id": "11923", "os": "Windows Server 2022"},
+                {"id": "118", "os": "Windows Server, version 20H2 (Server Core Installation)"},
+                {"id": "119", "os": "Windows Server 2022"},
                 #{"id": "11924", "os": "Windows Server 2022 (Server Core installation)"},
-                {"id": "9312", "os": "Windows Server 2008 for 32-bit Systems Service Pack 2"},
-                {"id": "9318", "os": "Windows Server 2008 for x64-based Systems Service Pack 2"},
-                {"id": "9344", "os": "Windows Server 2008 for x64-based Systems Service Pack 2 (Server Core installation)"}]
+                {"id": "93", "os": "Windows Server 2008 for 32-bit Systems Service Pack 2"},
+                {"id": "93", "os": "Windows Server 2008 for x64-based Systems Service Pack 2"},
+                {"id": "93", "os": "Windows Server 2008 for x64-based Systems Service Pack 2 (Server Core installation)"}]
 
 def get_kb_from_cve(cve):
     r = requests.get("https://api.msrc.microsoft.com/cvrf/v2.0/Updates('" + cve + "')").json()
@@ -54,12 +54,13 @@ def get_kb_from_cve(cve):
                                 #print(kb)
                             #version = (r.getElementsByTagName("vuln:ProductID")[0]).childNodes[0].data
                             version = (r.getElementsByTagName("vuln:ProductID")[0]).childNodes[0].data
-                            version_effective = version
-                            '''
+                            version_effective = False
+                            
                             for pids in product_ids:
-                                if version.startswith(pids["id"][:-3]):
+                                if version.startswith(pids["id"]):
                                     version_effective = pids["os"]
-                            '''
+                                if version_effective == pids["os"]: break
+                            
                             if(version_effective):
                                 if kb:
                                     el = {"CVE": cve, "Title": cve_title, "Patch": kb, "OS": version_effective}
